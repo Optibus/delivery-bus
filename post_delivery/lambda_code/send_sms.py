881799e994bfca6600f6f75c70b81699ad81164d
Eran Davidi <eran@optibus.com>
2018-12-04 20:41:15 +0200

    Update sender ID

--- post_delivery/lambda_code/send_sms.py
+++ post_delivery/lambda_code/send_sms.py
@@ -8,8 +8,14 @@ class SMS(object):
     @staticmethod
     def send_sms(message):
         client = boto3.client('sns',
-                              aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-                              aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                              # aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                              # aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
                               region_name='us-east-1')
         client.publish(Message=message,
-                       TopicArn="arn:aws:sns:us-east-1:008770191051:hackaton-delivery-bus")
+                       TopicArn="arn:aws:sns:us-east-1:008770191051:hackaton-delivery-bus",
+                       MessageAttributes={
+                           'AWS.SNS.SMS.SenderID': {
+                               'DataType': 'String',
+                               'StringValue': 'Deliverit'
+                           }
+                       })
